f89717d450632511b2cee06d9c410f80f05a9f34
soyjoseroberto <jrodriguez18@gmail.com>
2023-08-28 17:11:54 -0400

    Completed coding lab

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
+import RemainingBudget from './components/Remaining';
 
 
 import { AppProvider } from './context/AppContext';
@@ -15,11 +20,20 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
+                    { // Budget component
+                    <div className='col-sm'>
+                        <Budget />
+                    </div> }    
+                
+                    { // Remaining component
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div> }        
 
-                        {/* Add Remaining component here under */}        
-
-                        {/* Add ExpenseTotal component here under */}        
+                    { // ExpenseTotal component
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div> }
                        
                         {/* Add ExpenseList component here under */}         
 
@@ -27,6 +41,18 @@ const App = () => {
 
                         {/* Add AllocationForm component here under */}        
 
+                </div>
+                <h3 className='mt-3'>Allocation</h3>
+                <div className='row '>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Change allocation</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <AllocationForm/>
+                    </div>
                 </div>
             </div>
         </AppProvider>
